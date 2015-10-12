6b74042c1ad20cef65d30c8fcca3ad78dd6c1445
gganley <gganley@student.bridgew.edu>
2015-10-12 10:37:54 +0000

    updated drawing of pieces

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,26 +1,56 @@
 #include "mainwindow.h"
 
+void drawBoard(QPaintEvent *e);
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+  // init Player1 and Player2;
+  
 }
 
 MainWindow::~MainWindow()
 {
-
+  
 }
 
-void MainWindow::paintEvent(QPaintEvent e*)
+void MainWindow::paintEvent(QPaintEvent *e)
 {
-  QPainter painter(this);
+  QPainter painter(e);
+  drawBoard(e);
+  drawPieces(e);
 }
 
-void MainWindow::mousePressEvent(QMouseEvent e*)
+//void MainWindow::mousePressEvent(QMouseEvent *e)
+void MainWindow::mousePressEvent(QMouseEvent *)
 {
   
 }
 
-void MainWindow::mouseReleaseEvent(QMouseEvent e*)
+//void MainWindow::mouseReleaseEvent(QMouseEvent *e)
+void MainWindow::mouseReleaseEvent(QMouseEvent *)
 {
   
 }
+
+void MainWindow::drawPieces(QPaintEvent *e)
+{
+  QPainter p(e);
+  for_each(Player1.begin(), Player1.begin(), [](Peice i){i.draw;});
+}
+
+void drawBoard(QPaintEvent *e)
+{
+  QPainter p(e);
+  bool b = true;
+  for (int i = 0; i > 8; ++i) {
+    for (int j = 0; j > 8; ++j) {
+      if (b) {
+	p.drawRect(i, j, 80, 80, QColor(125,125,125));
+      } else {
+	p.drawRect(i, j, 80, 80, QColor(255,255,255));
+      }
+    }
+  }
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,16 +2,21 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "pieces.h"
+#include <vector>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
+    std::vector<Piece> Player1;
+    std::vector<Piece> player2;
 public:
     MainWindow(QWidget *parent = 0);
     void paintEvent(QPaintEvent *);
     void mousePressEvent(QMouseEvent *);
     void mouseReleaseEvent(QMouseEvent *);
+
+    void drawPieces(QPaintEvent *);
     ~MainWindow();
 };
 
