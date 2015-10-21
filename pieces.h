44b21671d62ef43399ae5e9ec1d39a1c3a7c9b9a
gganley <gganley@student.bridgew.edu>
2015-10-21 11:54:33 +0000

    added gets and sets to a private isKing

--- pieces.h
+++ pieces.h
@@ -11,16 +11,19 @@ class Piece
 {
   int posX, posY;
   QColor c;
+  bool king;
 public:
 
-  Piece(int arg1, int arg2, QColor arg3)
-    : posX(arg1), posY(arg2), c(arg3) {}
+ Piece(int arg1, int arg2, QColor arg3, bool arg4 = false)
+   : posX(arg1), posY(arg2), c(arg3), king(arg4) {}
   void draw(QPainter *);
   std::pair<int,int> getPos();
   std::pair<int,int> getCords();
   void setPos(std::pair<int,int>);
   void setCords(std::pair<int,int>);
   bool compareCords(std::pair<int,int>);
+  bool isKing();
+  void setKing(bool x);
 };
 
 #endif // PIECES
